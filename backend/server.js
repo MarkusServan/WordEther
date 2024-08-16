f3dee2224dcf71cdcdcf1cfafb558f6f816747af
Markus <markase@ntnu.no>
2024-08-16 14:43:23 +0200

    Serve static react after routes declared

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,7 @@ const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-});
+
 
 // CORS setup - allow requests from localhost during development, and from the app domain in production
 const allowedOrigins = ['http://localhost:3000', 'https://word-ether2-642016a371b5.herokuapp.com'];
@@ -124,14 +121,12 @@ router.delete('/poems/:id', async (req, res) => {
 
 app.use(router);
 
-// Serve static files from the React app
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
